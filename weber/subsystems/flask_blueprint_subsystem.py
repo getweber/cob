90fb40792720c5f56844817fb811bf68a3eec9e6
Rotem Yaari <vmalloc@gmail.com>
2016-05-26 21:57:39 +0300

    Work on blueprints

--- weber/subsystems/flask_blueprint_subsystem.py
+++ weber/subsystems/flask_blueprint_subsystem.py
@@ -10,12 +10,11 @@ class FlaskBlueprintSubsystem(SubsystemBase):
         super(FlaskBlueprintSubsystem, self).activate()
 
         for module in self.modules:
-            for blueprint_config in module.config['blueprints']:
+            for blueprint_config in module.config.get('blueprints', ()):
                 bp = module.load_python_symbol_by_name(blueprint_config['blueprint'])
                 print('blueprint', bp, 'in', blueprint_config['mountpoint'])
 
         gossip.register(self.configure_app, 'weber.configure_flask_app')
 
     def configure_app(self, app):
-        import pudb
-        pudb.set_trace()
+        pass
