f6589b212f7ca704b98eda102344ea275d7c1016
Rotem Yaari <vmalloc@gmail.com>
2016-09-13 22:00:06 +0300

    Improve bootstrapping

--- cob/bootstrapping.py
+++ cob/bootstrapping.py
@@ -10,7 +10,9 @@ from .defs import COB_CONFIG_FILE_NAME
 _logger = logbook.Logger(__name__)
 
 _PREVENT_REENTRY_ENV_VAR = 'COB_NO_REENTRY'
-_VIRTUALENV_PATH = '.cob-env'
+_COB_DEVELOP_MODE = 'COB_DEVELOP'
+_COB_REFRESH_ENV = 'COB_REFRESH_ENV'
+_VIRTUALENV_PATH = '.cob/env'
 
 def ensure_project_bootstrapped():
     if not os.path.isfile(COB_CONFIG_FILE_NAME):
@@ -23,16 +25,25 @@ def ensure_project_bootstrapped():
     _reenter()
 
 def _ensure_virtualenv():
-    if os.path.exists(os.path.join(_VIRTUALENV_PATH, 'bin', 'python')):
+    if os.path.exists(os.path.join(_VIRTUALENV_PATH, 'bin', 'python')) and _COB_REFRESH_ENV not in os.environ:
         _logger.trace('Virtualenv already seems bootstrapped. Skipping...')
         return
     _logger.trace('Creating virtualenv in {}', _VIRTUALENV_PATH)
+    venv_parent_dir = os.path.dirname(os.path.abspath(_VIRTUALENV_PATH))
+    if not os.path.isdir(venv_parent_dir):
+        os.makedirs(venv_parent_dir)
     venv.create(_VIRTUALENV_PATH)
-    _virtualenv_pip_install(['-e', os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))])
+    subprocess.check_call([os.path.join(_VIRTUALENV_PATH, 'bin', 'python'), '-m', 'ensurepip'])
+    if _COB_DEVELOP_MODE in os.environ:
+        _logger.trace('Using development version of cob')
+        _virtualenv_pip_install(['-e', os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))])
+    else:
+        _logger.trace('Installing cob form Pypi')
+        _virtualenv_pip_install(['-U', 'cob'])
 
 def _virtualenv_pip_install(argv):
     _logger.trace('Installing cob in virtualenv...')
-    subprocess.check_call([os.path.join(_VIRTUALENV_PATH, 'bin', 'pip'), 'install', *argv])
+    subprocess.check_call([os.path.join(_VIRTUALENV_PATH, 'bin', 'python'), '-m', 'pip', 'install', *argv])
 
 def _reenter():
     argv = sys.argv[:]
--- cob/cli/testserver.py
+++ cob/cli/testserver.py
@@ -2,6 +2,7 @@ import click
 import logbook
 
 from ..app import build_app
+from ..bootstrapping import ensure_project_bootstrapped
 
 _logger = logbook.Logger(__name__)
 
@@ -9,5 +10,6 @@ _logger = logbook.Logger(__name__)
 @click.command()
 @click.option('-p', '--port', type=int, default=5000)
 def testserver(port):
+    ensure_project_bootstrapped()
     flask_app = build_app()
     flask_app.run(port=port)
