2a1708065c760e168341edee15b813f0c4dafdc6
Rotem Yaari <vmalloc@gmail.com>
2016-12-13 10:41:14 +0200

    Fix bootstrapping issue

--- cob/cli/utils.py
+++ cob/cli/utils.py
@@ -1,11 +1,13 @@
 import functools
 
 from ..app import build_app
+from ..bootstrapping import ensure_project_bootstrapped
 
 
 def appcontext_command(func):
     @functools.wraps(func)
     def new_func(*args, **kwargs):
+        ensure_project_bootstrapped()
         with build_app().app_context():
             return func(*args, **kwargs)
 
