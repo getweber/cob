5a686499c6b195676ee2a2b190ec38e5a9ec5035
sivanbecker <sivanbecker@gmail.com>
2017-03-28 22:27:55 +0300

    cob automatically adds consumers for queues that are specifically mentioned in tasks + develop opens 4 tmux windows i need

--- cob/celery_utils.py
+++ cob/celery_utils.py
@@ -1,16 +1,24 @@
 from celery import Celery
 from kombu import Queue
+from cob.project import get_project
+import logbook
 cobcelery = Celery('cobtasks')
 
 # crontab celery schedule syntax:
 # http://docs.celeryproject.org/en/latest/userguide/periodic-tasks.html#crontab-schedules
 
+################################3
+# TURN IT TO CLASS
+################################
+
+_logger = logbook.Logger(__name__)
 
 cobcelery.config_from_object('celeryconfig')
 
 def task(*args, **kwargs):
     def task_wrap(func):
-        kwargs.update(general_task_update_conf(func, kwargs))
+        kwargs.update(non_periodic_task_update_conf(func, kwargs))
+        parse_general_task_args(kwargs)
         return cobcelery.task(func, **kwargs)
     return task_wrap
 
@@ -18,11 +26,12 @@ def task(*args, **kwargs):
 def periodic_task(*args, **kwargs):
     def periodic_task_wrap(func):
         kwargs.update(parse_simple_periodic_args(func, kwargs))
+        parse_general_task_args(kwargs)
         return cobcelery.task(func, **kwargs)
     return periodic_task_wrap
 
 
-def general_task_update_conf(func, *args, **kwargs):
+def non_periodic_task_update_conf(func, *args, **kwargs):
     if 'name' not in kwargs:
         return {'name':'_cob.tasks.%s' % func.__name__}
     else:
@@ -31,13 +40,13 @@ def general_task_update_conf(func, *args, **kwargs):
 def parse_simple_periodic_args(func, kwargs):
     '''
     for simple cases where the user
-    proveded "every" and "args" for the periodic task
+    provided "every" and "args" for the periodic task
     we'll parse manually and create the beat_schedule dict update
     in all other cases, the user will have to provide a full
     beat schedule dict that will be updated in cobcelery.conf
     '''
     global cobcelery
-    kwargs.update(general_task_update_conf(func, kwargs))
+    kwargs.update(non_periodic_task_update_conf(func, kwargs))
     if 'every' in kwargs:
             cobcelery.conf.beat_schedule.update({
                 'run-every-{}-seconds'.format(kwargs.get('every')): {
@@ -55,3 +64,23 @@ def parse_simple_periodic_args(func, kwargs):
         kwargs.pop('schedule', None)
 
     return kwargs
+
+def parse_general_task_args(kwargs):
+    '''
+    applicable for non periodic and periodic tasks
+    for args like queue=.. we would like to gather inforamtion
+    and keep it in mind for different reasons like:
+    1. when running cob develop ,running celery worker command states which queues
+       to monitor and if we know which queues are targeted in all tasks, we know automatically
+       which on to monitor.
+
+    '''
+    global cobcelery
+    if 'queue' in kwargs:
+        get_project().config.setdefault('celery_queues',[]).append(kwargs.get('queue'))
+        _logger.info("Adding celery consumer")
+        cobcelery.control.add_consumer(kwargs.get('queue'))
+        # consumers_data = cobcelery.control.inspect().ping()
+        # for c in consumers_data:
+            # cobcelery.control.add_consumer(c)
+        # import pudb;pudb.set_trace()
