108ad2a16eabd78f01a64984c96777ca09e9482c
Rotem Yaari <vmalloc@gmail.com>
2017-03-29 12:58:31 +0300

    Fix model unit test

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,5 +2,5 @@ from .project import Project
 
 def test_simple_models():
     proj = Project('models1')
-    proj.cmd('cob db createall')
+    proj.cob_cmd('db createall')
     assert proj.on('/index/list_models').returns_json([])
