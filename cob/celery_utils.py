961295023c5a2ccccdec00f42e81df3eeba2f227
Rotem Yaari <vmalloc@gmail.com>
2017-04-03 23:27:16 +0300

    Basic tasks working on worker properly

--- cob/celery_utils.py
+++ cob/celery_utils.py
@@ -1,111 +1,22 @@
+from .app import build_app
+
 from celery import Celery
 from celery.loaders.base import BaseLoader
 
 
+# Cob's loader. Used to correctly locate tasks
 
-class CobCelery(object):
+class CobLoader(BaseLoader):
 
-    def __init__(self, celery_inst):
-        self.cobcelery = celery_inst
-        self.task_list = []
-
-    def load_config(self, project):
-        '''
-        By default, cob holds some basic configuration values like
-           - broker url
-           - results backend
-        these values are stored in .cob-project.yml and if needed
-        can be altered manually by the user.
-        '''
-        self.cobcelery.conf.broker_url = project.config['celery']['broker_url']
-        self.cobcelery.conf.result_backend = project.config['celery']['result_backend']
-
-    def register_tasks(self):
-        for task in self.task_list:
-            self.cobcelery.register_task(task)
-
-    def update_celery_task_name(self, func, kwargs):
-        '''
-        this will make sure the celery config contains
-        a task name so that tasks are registered exactly
-        by the name we expect.
-        '''
-        import pudb;pudb.set_trace()
-        if not kwargs.get('name'):
-            return {'name': '%s.%s' % (func.__module__, func.__name__)}
-        else:
-            return {}
-
-
-    def add_queue_consumer(self, kwargs):
-        '''
-        in cases where the user provided a queue name
-        in the task args, we need to assign a worker
-        that will consume from this queue.
-        '''
-        if kwargs.get('queue'):
-            self.cobcelery.control.add_consumer(kwargs.get('queue'))
-
-
-    def periodic_task_parse_scheduling_args(self, func, kwargs):
-        '''
-        for now we support these args:
-        'every': user may add every=100s and the task will be sent every 100s.
-        'schedule': user provides a dict
-
-                  'task title': {
-                      'schedule': <num of seconds> or crontab(...),
-                      'args': (tuple of args),
-                  }
-
-             -> this function will just add a 'task': <task name>
-                to the dict and update celery.conf.beat_schedule with
-                the new task and its schedule
-        '''
-        # parse 'every' arg
-        if kwargs.get('every'):
-            self.cobcelery.conf.beat_schedule.update({
-                'run-every-{}-seconds'.format(kwargs.get('every')): {
-                    'task': kwargs.get('name'),
-                    'schedule':kwargs.get('every'),
-                    'args':kwargs.get('args'),
-                }
-            })
-            kwargs.pop('every', None)
-            kwargs.pop('args', None)
-
-        # parse 'schedule' arg
-        if kwargs.get('schedule'):
-            for k in kwargs['schedule'].keys():
-                kwargs['schedule'][k]['task'] = kwargs.get('name')
-                self.cobcelery.conf.beat_schedule.update(kwargs.get('schedule'))
-            kwargs.pop('schedule', None)
-
-        return kwargs
-
-
-
-def task(*args, **kwargs):
-    def task_wrap(func):
-        kwargs.update(celery_app.update_celery_task_name(func, kwargs))
-        celery_app.add_queue_consumer(kwargs)
-        task = celery_app.cobcelery.task(func, **kwargs)
-        celery_app.task_list.append(task)
-        return task
-    return task_wrap
+    def on_worker_init(self):
+        # this will make the tasks grains to be properly loaded and discovered
+        build_app()
 
 
 def periodic_task(*args, **kwargs):
-    def periodic_task_wrap(func):
-        kwargs.update(celery_app.update_celery_task_name(func, kwargs))
-        kwargs.update(celery_app.periodic_task_parse_scheduling_args(func, kwargs))
-        celery_app.add_queue_consumer(kwargs)
-        task = celery_app.cobcelery.task(func, **kwargs)
-        celery_app.task_list.append(task)
-        return task
-    return periodic_task_wrap
+    raise NotImplementedError()  # pragma: no cover
+
+celery_app = Celery('cob-celery', loader=CobLoader)
 
 
-
-celery_inst = Celery('cobtasks')
-celery_app = CobCelery(celery_inst)
+task = celery_app.task
--- cob/subsystems/tasks_subsystem.py
+++ cob/subsystems/tasks_subsystem.py
@@ -1,8 +1,6 @@
 import logbook
 
 from .base import SubsystemBase
-from ..celery_utils import celery_app
-
 _logger = logbook.Logger(__name__)
 
 
@@ -11,12 +9,12 @@ class TasksSubsystem(SubsystemBase):
     NAME = 'tasks'
 
 
-    def configure_grain(self, grain, flask_app):
-        _logger.trace('Found tasks grain: {}', grain)
-        # celery_app.cobcelery.conf.imports = ('%s' % grain.path.split('/')[-1].split('.')[0])
-        m = grain.load()
-        celery_app.register_tasks()
-
-
     def activate(self, flask_app):
-        celery_app.load_config(self.project)
+        from ..celery_utils import celery_app
+        celery_app.conf.broker_url = self.project.config['celery']['broker_url']
+
+    def configure_grain(self, grain, flask_app):
+        from ..celery_utils import celery_app
+        module = grain.load()
+        import pudb
+        pudb.set_trace()
