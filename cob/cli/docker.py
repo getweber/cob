16bbb6c01e076413767f3526844aae2673b8324e
Rotem Yaari <vmalloc@gmail.com>
2017-04-12 23:31:50 +0300

    Better handling for COB_DEVELOP mode in docker

--- cob/cli/docker.py
+++ cob/cli/docker.py
@@ -1,7 +1,9 @@
 import os
 import click
 import logbook
+import shutil
 import subprocess
+from tempfile import mkdtemp
 
 from jinja2 import Template
 from pkg_resources import resource_string
@@ -37,14 +39,30 @@ def generate():
     proj = get_project()
     dockerfile_template = Template(resource_string(
         "cob", "Dockerfile.j2").decode("UTF-8"))
+
+    if is_develop():
+        sdist_file_name = _build_cob_sdist()
+
     with open(".Dockerfile", "w") as f:
         f.write(dockerfile_template.render(
             project=proj,
             deployment_base_image='ubuntu:latest',
             python_version='3.6',
             is_develop=is_develop(),
-            cob_root=cob_root(),
+            cob_sdist_filename=sdist_file_name,
+            cob_root=cob_root() if is_develop() else None,
             user_steps=_get_user_steps()))
+
+def _build_cob_sdist():
+    tmpdir = mkdtemp()
+    try:
+        subprocess.check_call('python setup.py sdist -d {}'.format(tmpdir), cwd=cob_root(), shell=True)
+        [distfile] = os.listdir(tmpdir)
+        returned = '.cob-sdist.tar.gz'
+        shutil.move(os.path.join(tmpdir, distfile), os.path.join(get_project().root, returned))
+    finally:
+        shutil.rmtree(tmpdir)
+    return returned
 
 
 @docker.command()
