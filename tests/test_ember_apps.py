e44b870700a2dd309a40923f690a224cbc9ab96c
Rotem Yaari <vmalloc@gmail.com>
2018-02-21 11:29:27 +0200

    Add tests for Ember apps not installed as root resource grains

--- tests/test_ember_apps.py
+++ tests/test_ember_apps.py
@@ -8,6 +8,12 @@ def test_ember_assets():
         for asset_uri in _iter_assets(app):
             assert app.get(asset_uri)
 
+def test_ember_non_root_uri():
+    with Project('ember_non_root_uri').server_context() as app:
+        assert app.get('/') == 'root resource here'
+        assert 'link rel="stylesheet"' in app.get('/ui/')
+
+
 def _iter_assets(app):
     page = app.get('/').text
     yield from re.findall(r'<link rel="stylesheet" href="([^"]*)">', page)
