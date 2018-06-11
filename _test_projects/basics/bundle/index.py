08d7305c9b823c09beaed6998544e3771dfbecda
Rotem Yaari <vmalloc@gmail.com>
2018-06-11 10:05:48 +0300

    basics test project - add configuration-dumping route

--- _test_projects/basics/bundle/index.py
+++ _test_projects/basics/bundle/index.py
@@ -1,6 +1,14 @@
 # cob: type=views mountpoint=/bundle/index
 from cob import route
+from cob.project import get_project
+
+from flask import jsonify
 
 @route('/')
 def index():
     return 'hey'
+
+
+@route('/config')
+def render_config():
+    return jsonify(get_project().config)
